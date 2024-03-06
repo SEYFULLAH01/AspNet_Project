3b6969f0af1ef6c8a1beec2041d042649688646b
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2024-03-06 16:52:20 +0300

    Part-2

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.FluentValidation;
+using DataAccessLayer.Entity_Framework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet_Project.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+        public IActionResult Index()
+        {
+            var values = productManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            ProductValidator validationRules = new ProductValidator();
+            ValidationResult results = validationRules.Validate(product);
+            if (results.IsValid)
+            {
+                productManager.TInsert(product);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = productManager.TGetById(id);
+            productManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var value = productManager.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            productManager.TUpdate(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
