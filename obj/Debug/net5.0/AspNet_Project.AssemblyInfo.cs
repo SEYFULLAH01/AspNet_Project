a2307b2c44124628b3d0cd4d1a1e1facc09b14a2
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2024-03-12 18:17:38 +0300

    Part-7
    Migrationda Bazı Revizeler Yapıldı

--- obj/Debug/net5.0/AspNet_Project.AssemblyInfo.cs
+++ obj/Debug/net5.0/AspNet_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNet_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b29b58660f19aacd87c5d17f7d2c1f635aa23268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+658fb0887d38d5cedacd83bfc604044309d41426")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNet_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNet_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
